0efa0e794dc037915c9604e357ebb922323d0528
Leandro Pinto <leandro.pnto@gmail.com>
2016-09-05 17:11:56 -0300

    Desafio: Validando campos da cerveja

--- src/main/java/br/com/leandro/brewer/model/Cerveja.java
+++ src/main/java/br/com/leandro/brewer/model/Cerveja.java
@@ -4,7 +4,7 @@ import br.com.leandro.brewer.validation.SKU;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -27,26 +27,37 @@ public class Cerveja implements Serializable {
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
 
+    @NotBlank(message = "A descrição é obrigatória")
     @Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
     private String descricao;
 
+    @NotNull(message = "O valor é obrigatório")
+    @DecimalMin(value = "0.01")
+    @DecimalMax(value = "9999999.99", message = "O valor da cerveja deve ser menor que R$ 9.999.999,99")
     private BigDecimal valor;
 
+    @NotNull(message = "O teor alcóolico é obrigatório")
+    @DecimalMax(value = "100.0", message = "o valor do teor alcóolico dever menor que 100")
     @Column(name = "teor_alcoolico")
     private BigDecimal teorAlcoolico;
 
+    @DecimalMax(value = "100.0", message = "A comissão dever ser igual ou menor que 100")
     private BigDecimal comissao;
 
+    @Max(value = 9999, message = "A quantidade em estoque deve ser menor que 9.999")
     @Column(name = "quantidade_estoque")
     private Integer quantidadeEstoque;
 
+    @NotNull(message = "A origem é obrigatória")
     @Enumerated(EnumType.STRING)
     private Origem origem;
 
+    @NotNull(message = "O sabor é obrigatório")
     @Enumerated(EnumType.STRING)
     private Sabor sabor;
 
 
+    @NotNull(message = "O estilo é obrigatório")
     @ManyToOne
     @JoinColumn(name = "codigo_estilo")
     private Estilo estilo;
